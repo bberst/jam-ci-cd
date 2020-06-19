a0fa3519d665e813d7cfd91ad390f0a27c94a92c
bberst <32759956+bberst@users.noreply.github.com>
2020-06-19 15:26:38 -0700

    correct deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,7 @@ export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 export BUCKET="${BUCKET}"
 export CLOUDFRONT_DISTRIBUTION_ID="${CLOUDFRONT_DISTRIBUTION_ID}"
 
-[[ -z "${AWS_ACCESS_KEY_ID}"]] && echo "AWS_ACCESS_KEY_ID is missing"
+[[ -z "${AWS_ACCESS_KEY_ID}"]] && echo 'AWS_ACCESS_KEY_ID is missing'
 
 aws s3 sync build/ "s3://${BUCKET}"
 aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --path "/*"
