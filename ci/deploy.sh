fa1bcb0ee462ce1020606d39cc7cb3e83e8cb342
bberst <32759956+bberst@users.noreply.github.com>
2020-06-19 15:45:14 -0700

    add --no-verify-ssl

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,5 +6,5 @@ export CLOUDFRONT_DISTRIBUTION_ID="${CLOUDFRONT_DISTRIBUTION_ID}"
 
 [[ -z "${AWS_ACCESS_KEY_ID}" ]] && echo 'AWS_ACCESS_KEY_ID is missing'
 
-aws s3 sync build/ "s3://${BUCKET}"
-aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --path "/*"
+aws s3 sync build/ "s3://${BUCKET}" --no-verify-ssl
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --path "/*" --no-verify-ssl
